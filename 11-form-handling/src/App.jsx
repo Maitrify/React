3cdac92ed3d9085a1f3b63a71deee8be01d844a3
Maitri Patel <maitripatel2705@gmail.com>
2026-01-09 23:25:16 +0530

    learned to prevent default behaviour in forms

---
+++ 11-form-handling/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const App = () => {
+  const sumbitHandler = (e) =>{
+    e.preventDefault()
+    console.log('form submitted')
+  }
+
+  return (
+    <div>
+      <form onSubmit={(e) =>{
+        sumbitHandler(e)
+      }}>
+        <input type="text" placeholder='enter your name' />
+       <button>Submit</button>
+      </form>
+      
+    </div>
+  )
+}
+
+export default App
